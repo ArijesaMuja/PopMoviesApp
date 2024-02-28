ed76bd71718dbed3d999e24e4d655ffa01bb0b41
ArijesaMuja <117906993+ArijesaMuja@users.noreply.github.com>
2024-02-28 15:01:36 -0800

    Update GenreView.swift

--- PopMoviesApp/Views/Detail/GenreView.swift
+++ PopMoviesApp/Views/Detail/GenreView.swift
@@ -5,4 +5,26 @@
 //  Created by arijesa on 28.2.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct GenreView: View {
+    
+    @EnvironmentObject var moviesVM: MoviesViewModel
+    var genre: Genre
+    var movies: [Movie]{
+        return moviesVM.movies.filter{$0.genre==genre.rawValue}
+    }
+    var body: some View {
+        ScrollView{
+            MovieList(movies: movies)
+        }
+        .navigationTitle(genre.rawValue+"s")
+    }
+}
+
+struct GenreView_Previews: PreviewProvider {
+    static var previews: some View {
+    GenreView(genre: Genre.comedy)
+            .environmentObject(MoviesViewModel())
+    }
+}
