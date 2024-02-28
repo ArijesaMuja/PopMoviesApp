361addb516d0a99d07278f884b4aa077ba1b54da
ArijesaMuja <117906993+ArijesaMuja@users.noreply.github.com>
2024-02-28 15:06:00 -0800

    Update MovieList.swift

--- PopMoviesApp/Views/Components/MovieList.swift
+++ PopMoviesApp/Views/Components/MovieList.swift
@@ -5,4 +5,40 @@
 //  Created by arijesa on 28.2.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MovieList: View {
+    var movies:[Movie]
+    var body: some View {
+        VStack{
+            HStack {
+                Text("\(movies.count)\(movies.count > 1  ? " movies" : " movie")")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                .opacity(0.7)
+            
+                Spacer()
+            }
+            
+            LazyVGrid(columns:[GridItem(.adaptive(minimum: 160),spacing:15)],spacing:15){
+                ForEach(movies){
+                    movie in
+                    NavigationLink(destination: MovieView(movie: movie)){
+                        MovieCard(movie: movie)
+                    }
+                }
+            }
+            .padding(.top)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct MovieList_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollView{
+            MovieList(movies: Movie.all)
+
+        }
+    }
+}
