82230ba2f64427199e4c24d4a149f108ce00ce78
ArijesaMuja <117906993+ArijesaMuja@users.noreply.github.com>
2024-02-28 14:55:39 -0800

    Update UserModel.swift

--- PopMoviesApp/Models/UserModel.swift
+++ PopMoviesApp/Models/UserModel.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+class UserModel: Identifiable{
+    public var id: Int64=0
+    public var name: String=""
+    public var image: String=""
+    public var description: String=""
+}
