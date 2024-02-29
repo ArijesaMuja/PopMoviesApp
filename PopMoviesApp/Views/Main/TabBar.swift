f3bf5c96a34441d1666a5baf62ca9a8bd0f494a9
ArijesaMuja <arijesa.muja@student.uni-pr.edu>
2024-02-28 19:06:13 -0800

    TabBar creation

--- PopMoviesApp/Views/Main/TabBar.swift
+++ PopMoviesApp/Views/Main/TabBar.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem{
+                    Label("Home", systemImage: "house")
+                }
+            GenresView()
+                .tabItem{
+                    Label("Genres", systemImage: "square.fill.text.grid.1x2")
+                }
+            AddImpressionsView()
+                .tabItem{
+                    Label("New", systemImage: "plus")
+                }
+            TopMoviesView()
+                .tabItem{
+                    Label("Favorites", systemImage: "heart")
+                }
+        }
+    }
+}
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+            .environmentObject(MoviesViewModel())
+    }
+}
