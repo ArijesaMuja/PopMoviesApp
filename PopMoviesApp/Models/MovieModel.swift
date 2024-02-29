01a7d8bb526ca27a3b711ce6695dc7ba635b53f9
ArijesaMuja <117906993+ArijesaMuja@users.noreply.github.com>
2024-02-29 06:01:22 -0800

    Update MovieModel.swift

--- PopMoviesApp/Models/MovieModel.swift
+++ PopMoviesApp/Models/MovieModel.swift
@@ -30,15 +30,30 @@ extension Movie{
     static let all:[Movie]=[
         Movie(
             name: "The Godfather",
-            image: "",
-            description: "Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando). When the don's youngest son, Michael (Al Pacino), reluctantly joins the Mafia, he becomes involved in the inevitable cycle of violence and betrayal. Although Michael tries to maintain a normal relationship with his wife, Kay (Diane Keaton), he is drawn deeper into the family business.",
+            image: "https://media.newyorker.com/photos/594044d90c240c2a1fd678ce/1:1/w_2105,h_2105,c_limit/970324_ra409.jpg",
+            description: "Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando).",
             genre: "Drama",
-            datePublished: "March 24, 1972"
+            datePublished: "1972-03-10"
             ),
         Movie(name: "Casablanca",
-            image:"https://www.seriouseats.com/thmb/K6VksPblIy1WK4EtaXBdydAJLwU=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2018__09__20180924-brown-butter-cake-vicky-wasik-21-a9014e0de9a94d13b6f7b80aedaf834d.jpg",
+            image:"https://static.wikia.nocookie.net/warner-bros-entertainment/images/b/b3/CasablancaPoster-Gold.jpg/revision/latest?cb=20200826163009",
             description: "Rick Blaine (Humphrey Bogart), who owns a nightclub in Casablanca, discovers his old flame Ilsa (Ingrid Bergman) is in town with her husband, Victor Laszlo (Paul Henreid). Laszlo is a famed rebel, and with Germans on his tail, Ilsa knows Rick can help them get out of the country.",
             genre: "Action",
-            datePublished: "2022-9-23")
+            datePublished: "2022-9-23"),
+        Movie(name: "Barbie",
+             image:"https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk0NDg3NzEyODkwMjM0Mzcz/margot-robbie-barbie-movie-2023.jpg",
+             description: "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
+             genre: "Adventure",
+             datePublished: "2023-10-10"),
+        Movie(name: "Upgraded",
+             image:"https://m.media-amazon.com/images/M/MV5BZWNhMjc1MzMtZGUyOC00NjhiLWJiM2YtMWNkMzU0YjgwZDU0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1000_.jpg",
+             description: "An aspiring art intern is invited on a last-minute work trip to London, where she meets a handsome stranger.",
+             genre: "Romance",
+             datePublished: "2024-01-08"),
+        Movie(name: "Cinderella",
+             image:"https://m.media-amazon.com/images/M/MV5BZTk3ZTEzNGUtZTcwYy00NmRmLWFhMGYtZjA4NWY1ZWI4MzMyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+             description: "After her father's death, Ella is tormented by her venomous stepmother and stepsisters. Her life changes dramatically when a crown prince, who is smitten with her, invites her to a royal ball.",
+             genre: "Adventure",
+             datePublished: "2022-01-10"),    
     ]
 }
