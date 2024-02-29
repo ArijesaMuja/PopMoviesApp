93a66f2740fe93f9bf1220f761d7650c872a6f33
ArijesaMuja <arijesa.muja@student.uni-pr.edu>
2024-02-28 17:43:56 -0800

    Initial Commit

---
+++ PopMoviesApp/PopMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopMoviesAppApp.swift
+//  PopMoviesApp
+//
+//  Created by arijesa on 28.2.24.
+//
+
+import SwiftUI
+
+@main
+struct PopMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
